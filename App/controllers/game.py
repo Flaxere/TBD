453d8250947b10f804e7ed36fcb88c70872de511
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-04-09 22:06:34 -0400

    Controller updated attempt 2

--- App/controllers/game.py
+++ App/controllers/game.py
@@ -2,7 +2,9 @@ from App.models import Game
 from App.database import db
 
 def NewGame():
-   
-   
+    new_game = Game()
+    db.session.add(new_game)
+    db.session.commit()
+    print (new_game)
     print("DODO")
-    return 
+    return new_game

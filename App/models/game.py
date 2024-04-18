6fa9df78403aafbaf114ba5820c43ba5e172c302
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-04-18 17:46:33 -0400

    Updated init route and added date_created to game model

--- App/models/game.py
+++ App/models/game.py
@@ -4,6 +4,7 @@ from App.database import db
 class Game(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   code = db.Column(db.String(4), nullable=False)
+  date_created = db.Column(db.DateTime(timezone=True), server_default=db.func.now())
   
   def __init__(self):
     self.code = self.generate_code()

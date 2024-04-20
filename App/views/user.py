b326ba1a648bdc9fe4b8a233da73f33b2a5ba218
Flaxere <123189890+Flaxere@users.noreply.github.com>
2024-04-20 17:20:35 -0400

    Upated each route for user settings

--- App/views/user.py
+++ App/views/user.py
@@ -52,62 +52,45 @@ def create_user_endpoint():
 def static_user_page():
   return send_from_directory('static', 'static-user.html')
 
-@user_views.route('/settings/<int:id>', methods=['GET'])
+@user_views.route('/settings', methods=['GET'])
 @jwt_required()
-def settings_page(id):
-    user = get_user(id)
-    if user!=jwt_current_user:
-        flash(f"Invalid User")
-        return redirect(url_for('index_views.index_page'))
-    return render_template('settings.html',user=user) 
+def settings_page():
+    return render_template('settings.html',user=jwt_current_user) 
 
-@user_views.route('/deleteAccount/<int:id>', methods=['GET'])
+@user_views.route('/deleteAccount', methods=['GET'])
 @jwt_required()
-def del_user(id):
-    if jwt_current_user.id != id:
-        flash(f"Invalid User")
-        return redirect(url_for('index_views.index_page'))
-    user = get_user(id)
-    name = user.username
-    test = delete_games(id)
+def del_user():
+    name = jwt_current_user.username
+    test = delete_games(jwt_current_user.id)
    
     if(test):
-        test2= delete_user(id)
+        test2= delete_user(jwt_current_user.id)
         flash(f"User {name} deleted...")
     else:
         flash(f"User {name} could not be deleted")
     return redirect(url_for('index_views.index_page'))
 
-@user_views.route('/changeUsername/<int:id>', methods=['POST'])
+@user_views.route('/changeUsername', methods=['POST'])
 @jwt_required()
-def change_uname(id):
-    if jwt_current_user.id != id:
-        flash(f"Invalid User")
-        return redirect(url_for('index_views.index_page'))
-    
+def change_uname():  
     data = request.form
-    user = get_user(id)
-    prevname = user.username
-    test = rename_user(id,data['username'])
+    prevname = jwt_current_user.username
+    test = rename_user(jwt_current_user.id,data['username'])
     if test:
-        flash(f"Username Changed from {prevname} to {user.username}")
+        flash(f"Username Changed from {prevname} to {jwt_current_user.username}")
     else:
         flash(f"The username {data['username']} has already been taken")
     return redirect(request.referrer)
 
-@user_views.route('/changePassword/<int:id>', methods=['POST'])
+@user_views.route('/changePassword', methods=['POST'])
 @jwt_required()
-def change_pass(id):
-    if jwt_current_user.id != id:
-        flash(f"Invalid User")
-        return redirect(url_for('index_views.index_page'))
+def change_pass():
     
     data = request.form
     if data['password1'] != data['password2']:
         flash("Please enter the same password")
         return redirect(request.referrer)
     
-    user = get_user(id)
-    test = change_password(id,data['password1'])
+    test = change_password(jwt_current_user.id,data['password1'])
     flash(f"Password Updated")
     return redirect(request.referrer)
